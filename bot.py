2c11cc2794b7a7a82eac38120a58de8b3abbbdc9
Miriam Lipkovich <miri.lippa@yahoo.com>
2020-09-22 01:15:13 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,4 @@
+import telegram
+bot = telegram.Bot(token = '1313430420:AAG24E5iOg0JyGVgxyC9eQha75tYxUSUunE')
+print(bot.get_me())
+{"first_name": "Toledo's Palace Bot", "username": "ToledosPalaceBot"}
