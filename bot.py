53dab9f8f0b7ba28a24843de7afa01b037d673d5
Miriam Lipkovich <miri.lippa@yahoo.com>
2020-09-26 19:01:19 +0300

    added webhook and functions

--- bot.py
+++ bot.py
@@ -1,18 +1,33 @@
 import telegram
-bot = telegram.Bot(token = '1313430420:AAG24E5iOg0JyGVgxyC9eQha75tYxUSUunE')
+import logging
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+TOKEN = '1313430420:AAG24E5iOg0JyGVgxyC9eQha75tYxUSUunE'
+bot = telegram.Bot(TOKEN)
 print(bot.get_me())
-{"first_name": "Toledo's Palace Bot", "username": "ToledosPalaceBot"}
-import logging 
+{"first_name": "Toledo's Palace Bot", "username": "ToledosPalaceBot"} 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
-
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-updater = Updater(token = '1313430420:AAG24E5iOg0JyGVgxyC9eQha75tYxUSUunE', use_context = True)
-dispatcher = updater.dispatcher
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-updater.start_polling()
+logger = logging.getLogger(__name__)
+PORT = int(os.environ.get('PORT', 5000))
 
 def start(update, context):
-	context.bot.send_message(chat_id = update.effective_chat_id, text = "I'm a bot, please talk to me!")
+	context.bot.send_message(chat_id = update.effective_chat_id, text = "Hello")
 
+def echo(update, context):
+	update.message.reply_text(update.message.text)
+
+def error(update, context):
+	logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+	updater = Updater(token = '1313430420:AAG24E5iOg0JyGVgxyC9eQha75tYxUSUunE', use_context = True)
+	dispatcher = updater.dispatcher
+	start_handler = CommandHandler('start', start)
+	dispatcher.add_handler(start_handler)
+	dispatcher.add_handler(error)
+	dispatcher.add_handler(MessageHandler(Filters.text, echo))
+	updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path = TOKEN)
+	updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
+
+if __name__ == '__main__':
+    main()
